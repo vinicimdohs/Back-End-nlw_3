43e56f4668e2d6bc2905420d7c00d93c1412ffa8
vinicius Fernandes <vinicim.fernandez1@gmail.com>
2020-10-16 00:58:32 -0300

    organizando as views,tratando os erros e validadando os campos com o yup

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,8 @@ import {request, Request,Response} from 'express';
 import {getRepository}from 'typeorm';
 import Orphanage from '../model/Orphanage'
 import orphanageview from '../views/orphanages_view';
+import * as Yup from 'yup';
+
 
 export default {
 
@@ -42,6 +44,34 @@ export default {
             const images = requestImages.map(image =>{
                 return {path: image.filename}
             })
+
+            const data = {
+                name,
+            latitude,
+            longitude,
+            about,
+            instruction,
+            opening_hours,
+            open_on_weekends,
+            images
+            };
+
+            const schema = Yup.object().shape({
+                name: Yup.string().required(),
+                latitude:Yup.number().required(),
+                longitude:Yup.number().required(),
+                about:Yup.string().required().max(300),
+                instruction:Yup.string().required(),
+                opening_hours:Yup.string().required(),
+                open_on_weekends:Yup.boolean().required(),
+                images: Yup.array(Yup.object().shape({
+                    path: Yup.string().required()
+                }))
+            });
+
+            await schema.validate(data,{
+                abortEarly:false,
+            });
 
             const orphanage = orphanagesRepository.create({
                 name,
